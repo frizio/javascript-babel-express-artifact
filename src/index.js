e486740167ba2317baa552fc1f126144939a9406
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-01 15:34:09 +0100

    Basic express server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { PORT } from "./config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 
-console.log("Demo ES modules");
-console.log("Env variable PORT: ", PORT);
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(PORT);
+
+console.log(`Server on port http://localhost:${PORT}`);
